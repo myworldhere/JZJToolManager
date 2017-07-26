d80dd0a811f32c07183ae64cd5232f41ebd7eebc
易骆驼 <ekiluotuo@ekiluotuotekiiMac.local>
2017-07-26 14:24:05 +0800

    项目初始化

---
+++ JZJToolManagerDemo/JZJToolManagerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JZJToolManagerDemo
+//
+//  Created by 易骆驼 on 2017/7/26.
+//  Copyright © 2017年 eluotuo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JZJToolManagerDemo/JZJToolManagerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JZJToolManagerDemo
+//
+//  Created by 易骆驼 on 2017/7/26.
+//  Copyright © 2017年 eluotuo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
