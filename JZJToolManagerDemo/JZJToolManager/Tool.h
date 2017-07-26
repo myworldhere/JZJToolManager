dbee0f9c6a56aa65ce044378249d9d6944829415
易骆驼 <ekiluotuo@ekiluotuotekiiMac.local>
2017-07-26 17:34:39 +0800

    添加新工具类

---
+++ JZJToolManagerDemo/JZJToolManager/Tool.h
@@ -0,0 +1,15 @@
+//
+//  Tool.h
+//  JZJToolManagerDemo
+//
+//  Created by 易骆驼 on 2017/7/26.
+//  Copyright © 2017年 eluotuo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Tool : NSObject
+
++(void)print;
+
+@end
---
+++ JZJToolManagerDemo/JZJToolManager/Tool.m
@@ -0,0 +1,18 @@
+//
+//  Tool.m
+//  JZJToolManagerDemo
+//
+//  Created by 易骆驼 on 2017/7/26.
+//  Copyright © 2017年 eluotuo. All rights reserved.
+//
+
+#import "Tool.h"
+
+@implementation Tool
+
++(void)print
+{
+    NSLog(@"%s",__FUNCTION__);
+}
+
+@end
